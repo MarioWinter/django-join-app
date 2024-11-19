b21a87723759656636659f4d9c5629904d5caa4d
Mario Winter <mariowinter.sg@gmail.com>
2024-11-19 13:28:13 +0100

    feat(tasks-app): add drf-writable-nested and update task serializer

--- tasks_app/api/serializers.py
+++ tasks_app/api/serializers.py
@@ -1,52 +1,22 @@
+from urllib import request
 from rest_framework import serializers
+from drf_writable_nested import WritableNestedModelSerializer
 # from django.contrib.auth.models import User
 
-from tasks_app.models import Task
+from tasks_app.models import Task, Subtasks
 # from contacts_app.api.serializers import ContactSerializer
 # from contacts_app.models import Contact
 
-# class SubtaskSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Subtask
-#         fields = ['id', 'subdone', 'subtitle', 'user']
+class SubtaskSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(source='user_id', read_only=True, default=serializers.CurrentUserDefault())
+    class Meta:
+        model = Subtasks
+        fields = ['id', 'subdone', 'subtitle', 'user']
 
 
-class TaskSerializer(serializers.ModelSerializer):
-    #assigned = ContactSerializer(many=True, read_only=True) 
-    #subtasks = SubtaskSerializer(many=True, read_only=True)
-
-     # Diese Felder werden für POST/PUT/PATCH verwendet
-    # subtask_id = serializers.PrimaryKeyRelatedField(
-    #     many=True, write_only=True, queryset=Subtask.objects.all(), source='subtasks'
-    # )
-    # assigned_id = serializers.PrimaryKeyRelatedField(
-    #     many=True, write_only=True, queryset=Contact.objects.all(), source='assigned'
-    # )
-
+class TaskSerializer(WritableNestedModelSerializer):    
+    subtasks = SubtaskSerializer(many=True, required=False)
     class Meta:
         model = Task
-        fields = ['id', 'bucket', 'title', 'description', 'assigned', 'duedate', 'prio', 'category', 'subtask', 'user']
+        fields = ['id', 'bucket', 'title', 'description', 'assigned', 'duedate', 'prio', 'category', 'subtasks', 'user']
         read_only_fields = ['user']
-
-    # def create(self, validated_data):
-    #     # subtasks = validated_data.pop('subtasks', [])
-    #     assigned = validated_data.pop('assigned', [])
-    #     task = Task.objects.create(**validated_data)
-    #     # task.subtasks.set(subtasks)
-    #     task.assigned.set(assigned)
-    #     return task
-
-    # def update(self, instance, validated_data):
-    #     # subtasks = validated_data.pop('subtasks', None)
-    #     assigned = validated_data.pop('assigned', None)
-        
-    #     for attr, value in validated_data.items():
-    #         setattr(instance, attr, value)
-        
-    #     # if subtasks is not None:
-    #     #     instance.subtasks.set(subtasks)
-    #     if assigned is not None:
-    #         instance.assigned.set(assigned)
-        
-    #     instance.save()
-    #     return instance
--- tasks_app/models.py
+++ tasks_app/models.py
@@ -1,34 +1,35 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import authenticate, get_user_model
 
-#from django.contrib.auth.models import User
+User = get_user_model()
 # from contacts_app.models import Contact
 
 
 class Task(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     bucket = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    # assigned = models.ManyToManyField(Contact, related_name='assigned_tasks', blank=True)
+    #assigned = models.ManyToManyField(Contact, related_name='assigned_tasks', blank=True)
     assigned = models.JSONField(default=list)
     duedate = models.CharField(max_length=10)
     prio = models.CharField(max_length=10)
     category = models.CharField(max_length=100)
-    subtask = models.JSONField(default=list)
+    #subtask = models.JSONField(default=list)
     
     def __str__(self):
         return self.title
 
 
-# class Subtask(models.Model):
-#     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     subdone = models.BooleanField(blank=True, default=False)
-#     subtitle = models.CharField(max_length=1000)
+class Subtasks(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subdone = models.BooleanField(blank=True, default=False)
+    subtitle = models.CharField(max_length=1000)
 
-#     def __str__(self):
-#         return self.subtitle
+    def __str__(self):
+        return self.subtitle
       
     
 # addedTasks.push({
