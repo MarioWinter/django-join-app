c84bd8f81db329cd4f1569ddde0d55623e75777a
Mario Winter <mariowinter.sg@gmail.com>
2024-12-10 21:23:34 +0100

    feat(contacts_app): Add email uniqueness validation in ContactSerializer and update trusted origins in settings

--- contacts_app/api/serializers.py
+++ contacts_app/api/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-
-#from django.contrib.auth.models import User
-
 from contacts_app.models import Contact
 User = get_user_model()
+
 class ContactSerializer(serializers.ModelSerializer):
-    #zum Testen
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), default=serializers.CurrentUserDefault())
     class Meta:
         model = Contact
         fields = ['id', 'name', 'email', 'phone', 'bgcolor', 'user']
+        
+    def validate_email(self, value):
+        user = self.context['request'].user
+        if Contact.objects.filter(user=user, email=value).exists():
+            raise serializers.ValidationError("This E-mail address already exists")
+        return value
