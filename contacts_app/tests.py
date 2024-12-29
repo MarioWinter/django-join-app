26543c15cd41aadb0943baf4953dde775038802a
Mario Winter <mariowinter.sg@gmail.com>
2024-12-29 19:55:20 +0100

    test: add test for retrieving contact list

--- contacts_app/tests.py
+++ contacts_app/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your tests here.
+class ContactListTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='testuser@gmail.com', password='werte12345')
+        self.client.force_authenticate(user=self.user)
+    
+    def test_get_contacts(self):
+        url = reverse('contact-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
