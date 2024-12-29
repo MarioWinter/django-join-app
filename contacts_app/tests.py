84427fb38a018a999a7e39eecea228d2e0e62c49
Mario Winter <mariowinter.sg@gmail.com>
2024-12-29 20:08:00 +0100

    test: add unit tests for contact list and detail retrieval

--- contacts_app/tests.py
+++ contacts_app/tests.py
@@ -2,14 +2,23 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
+
+from .models import Contact
+
 User = get_user_model()
 
 class ContactListTest(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(email='testuser@gmail.com', password='werte12345')
         self.client.force_authenticate(user=self.user)
+        self.contact = Contact.objects.create(username='Max Mustermann', user=self.user, email="maxmustermann@gmail.com", bgcolor="#FFFFFF", phone="+4934567890")
     
-    def test_get_contacts(self):
+    def test_get_contacts_list(self):
         url = reverse('contact-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_contact_detail(self):
+        url = reverse('contact-detail', kwargs={'pk': self.contact.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
