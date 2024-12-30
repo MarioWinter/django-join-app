4d55e2ef53ff7d5b0a07b1150912294a12e8e274
Mario Winter <mariowinter.sg@gmail.com>
2024-12-30 23:05:36 +0100

    test: add POST method test for contact list endpoint

--- contacts_app/tests.py
+++ contacts_app/tests.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 
 from .models import Contact
@@ -10,8 +11,10 @@ User = get_user_model()
 class ContactListTest(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(email='testuser@gmail.com', password='werte12345')
-        self.client.force_authenticate(user=self.user)
+        self.token = Token.objects.create(user=self.user)
         self.contact = Contact.objects.create(username='Max Mustermann', user=self.user, email="maxmustermann@gmail.com", bgcolor="#FFFFFF", phone="+4934567890")
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
     
     def test_get_contacts_list(self):
         url = reverse('contact-list')
@@ -34,4 +37,17 @@ class ContactListTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, expected_data)
         self.assertNotEqual(response.data.get('type', None), 'user')
+        
+    def test_post_contact_list(self):
+        url = reverse('contact-list')
+        data = {
+            'username': 'Cody Mustermann',
+            'email': 'codymustermann@gmail.com',
+            'phone': '+4934567890',
+            'bgcolor': '#FFFFFF',}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Contact.objects.count(), 2)
+        new_contact = Contact.objects.get(username='Cody Mustermann')
+        self.assertEqual(new_contact.email, 'codymustermann@gmail.com')
 
